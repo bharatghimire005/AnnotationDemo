2a377069265af36f15482c2ca0d80753e1802196
Bharat Ghimire <bharat.ghimire@tothenew.com>
2017-03-29 11:29:45 +0530

    Added code for the nullness example

--- app/src/main/java/com/dentor/annotationdemo/MainActivity.java
+++ app/src/main/java/com/dentor/annotationdemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.dentor.annotationdemo;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Toast.makeText(this, "" + findTotalLength("Annotation", "Demo"), Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "" + findTotalLength(null, null), Toast.LENGTH_SHORT).show();
+        // Toast.makeText(this, "" + findTotalLength("Annotation"), Toast.LENGTH_SHORT).show();
+        // Toast.makeText(this, "" + findTotalLength(null), Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    @Nullable
+    public int findTotalLength(@NonNull String s1, @NonNull String s2) {
+        return new StringBuilder(s1).append(s2).length();
+
+    }
+
+    @Nullable
+    public int findTotalLength(@Nullable String s1) {
+        if (s1 == null) return 0;
+        else return new StringBuilder(s1).length();
 
     }
 }
