34b7ee74754b6666f7d7d176e26b2b20e31f2282
Bharat Ghimire <bharat.ghimire@tothenew.com>
2017-03-29 15:20:17 +0530

    Added example of Value Constraint demo

--- app/src/main/java/com/dentor/annotationdemo/MainActivity.java
+++ app/src/main/java/com/dentor/annotationdemo/MainActivity.java
@@ -1,14 +1,41 @@
 package com.dentor.annotationdemo;
 
+import android.support.annotation.FloatRange;
+import android.support.annotation.IntRange;
+import android.support.annotation.Size;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    int[] arr = new int[20];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intRange(200);
+        floatRange(10);
+        String a = "Bhara";
+        lengthCount(a);
+    }
+
+    public void intRange(@IntRange(from = 0, to = 200) int num) {
+
+        Log.e(TAG, num + "");
+    }
+
+    public void floatRange(@FloatRange(from = 0.0, to = 10.0) float num) {
+
+        Log.e(TAG, num + "");
 
     }
+
+    public void lengthCount(@Size(min = 3) String arr) {
+// do something
+    }
+
+
 }
